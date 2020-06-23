27fdd7b01a36f2e53bebd575e6f3047b78bbaae8
Hagimoli <hagimoli0123@gmail.com>
2020-06-23 17:32:38 +0700

    Begin simple with C

--- Logic Building & Elementary Programing(EPC)/Enter_and_display_1_value.c
+++ Logic Building & Elementary Programing(EPC)/Enter_and_display_1_value.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <conio.h>
 void main(){
-	printf("hello Batch 151 \n");
+	printf("Hello Everybody \n");
 	printf("---Nhap mot so bat ki va in ra man hinh--- \n");
 	int a;
     printf("\n Hay nhap 1 so a: ");
